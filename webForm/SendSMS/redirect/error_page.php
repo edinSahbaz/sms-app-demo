18337ecdaafc5aefa5612beba385863168e63c32
edin.sahbaz <edin.sahbaz@hotmail.com>
2019-04-18 18:11:06 +0200

    Changes

--- webForm/SendSMS/redirect/error_page.php
+++ webForm/SendSMS/redirect/error_page.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION["error"])){
     <script>
         setTimeout(function(){
             window.location = "../form/index.html";
-        }, 15550000);
+        }, 10000);
     </script>
 </head>
 <body>
@@ -22,12 +22,7 @@ if (!isset($_SESSION["error"])){
     <div class="toast__cell">
         <div class="toast toast--yellow add-margin">
             <div class="toast__icon">
-                <svg version="1.1" class="toast__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 301.691 301.691" style="enable-background:new 0 0 301.691 301.691;" xml:space="preserve">
-<g>
-	<polygon points="119.151,0 129.6,218.406 172.06,218.406 182.54,0  "></polygon>
-	<rect x="130.563" y="261.168" width="40.525" height="40.523"></rect>
-</g>
-    </svg>
+
             </div>
             <div class="toast__content">
                 <p class="toast__type">Error!</p>
@@ -36,14 +31,5 @@ if (!isset($_SESSION["error"])){
         </div>
     </div>
 </div>
-<script>
-    jQuery(document).ready(function(){
-        jQuery('.toast__close').click(function(e){
-            e.preventDefault();
-            var parent = $(this).parent('.toast');
-            parent.fadeOut("slow", function() { $(this).remove(); } );
-        });
-    });
-</script>
 </body>
 </html>
